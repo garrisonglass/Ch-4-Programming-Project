fd87f2e9984518a33c8f7991f28d66e19304ec17
garrisonglass <garrison.glass@gmail.com>
2025-10-28 21:15:51 -0400

    Edited String

--- Ch 4 Programming Project.cpp
+++ Ch 4 Programming Project.cpp
@@ -1,20 +1,67 @@
-// Ch 4 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Ch 4 Programming Project.cpp : Shipping Cost
 
-#include <iostream>
+#include <iostream>  
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double weight, shippingCost, distance;
+
+    cout << "Enter the weight of package (in kg): ";
+    cin >> weight;
+
+    if (weight <= 0)
+    {
+        cout << "Invalid weight. Please enter a valid wieght." << endl;
+    }
+    else
+
+
+
+    {
+        if (weight <= 2.0)
+        {
+            shippingCost = 1.10; // Rate per 500 miles shipped <= 2kg  
+        }
+        else if (weight > 2.0 && weight <= 6.0)//The > 2.0 may be redundant 
+        {
+            shippingCost = 2.20; // Rate per 500 miles shipped 2> and <= 6kg  
+        }
+        else if (weight > 6.0 && weight <= 10.0)
+        {
+            shippingCost = 3.70; // Rate per 500 miles shipped 6> and <= 10kg           
+        }
+        else if (weight > 10.0 && weight <= 20.0)
+        {
+            shippingCost = 4.80; // Rate per 500 miles shipped 10> and <= 20kg  
+        }
+        else
+        {
+            cout << "Package too heavy" << endl;; // Output for packages > 20kg  
+            return 0;
+        }
+        cout << "The shipping cost for a package weighing " << weight << " kg is $" << shippingCost << " per 500  miles." << endl;
+    }
+    cout << "Enter distance in miles\n";
+    cin >> distance;
+
+    double total;
+
+    if (distance < 10)
+    {
+        cout << "Take it yourself!\n";
+    }
+    else if (distance > 3000)
+    {
+        cout << "Sorry, We don't go that far.\n" << endl;
+        return 0;
+    }
+    else
+    {
+        int distanceFactor = static_cast<int>((distance + 499) / 500); // Round up to next 500 miles. Truncates 
+        total = shippingCost * distanceFactor;
+        cout << "The Total cost for shipping is $" << total << " for a distance of " << distance << " miles\n" << endl;
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
