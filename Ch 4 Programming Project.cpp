f369dd3994e1bb896ba6187db4c9705acf511f9d
garrisonglass <garrison.glass@gmail.com>
2025-10-28 21:28:58 -0400

    Input validation

--- Ch 4 Programming Project.cpp
+++ Ch 4 Programming Project.cpp
@@ -1,20 +1,69 @@
-// Ch 4 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Ch 4 Programming Project.cpp : 
 
-#include <iostream>
+#include <iostream> 
+
+using namespace std;
+
+// Function to check if a year is a leap year  
+bool isLeapYear(int year)
+{
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+// Function to get the number of days in a month  
+int getDaysInMonth(int month, int year)
+{
+    switch (month)
+    {
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        return 31;
+        break;
+    case 4: case 6: case 9: case 11:
+        return 30;
+    case 2:
+        return isLeapYear(year) ? 29 : 28;
+    default:
+        return -1;//Invalid month  
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int month, year;
+
+    cout << "Enter month (1-12): ";
+    cin >> month;
+    cout << "Enter year: ";
+    cin >> year;
+
+    int days = getDaysInMonth(month, year);
+
+    if (days == -1)
+    {
+        cout << "That's not a real month!" << endl;
+    }
+    else if (year <= -1)
+    {
+        cout << "Is that BCE?\n";
+    }
+    else
+    {
+        if (month == 1)
+        {
+            cout << "The number of days in the " << month << "st month of year " << year << " is: " << days << endl;
+        }
+        else if (month == 2)
+        {
+            cout << "The number of days in the " << month << "nd month of year " << year << " is: " << days << endl;
+        }
+        else if (month == 3)
+        {
+            cout << "The number of days in the " << month << "rd month of year " << year << " is: " << days << endl;
+        }
+        else
+        {
+            cout << "The number of days in the " << month << "th month of year " << year << " is: " << days << endl;
+        }
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
